12bc1acbe0b6ab22eca0d77f15f92b21a320c374
Alexei Ledenev <alexei.led@gmail.com>
2024-09-22 11:43:29 +0300

    implement workloadautoscaler CRD

--- api/v1alpha1/workloadautoscaler_types.go
+++ api/v1alpha1/workloadautoscaler_types.go
@@ -25,17 +25,71 @@ import (
 
 // WorkloadAutoscalerSpec defines the desired state of WorkloadAutoscaler
 type WorkloadAutoscalerSpec struct {
-	// INSERT ADDITIONAL SPEC FIELDS - desired state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	Target               TargetSpec       `json:"target"`
+	VPAReference         VPAReference     `json:"vpaReference"`
+	UpdateFrequency      Duration         `json:"updateFrequency"`
+	AllowedUpdateWindows []UpdateWindow   `json:"allowedUpdateWindows"`
+	StepSize             ResourceRequests `json:"stepSize"`
+	GracePeriod          Duration         `json:"gracePeriod"`
+}
 
-	// Foo is an example field of WorkloadAutoscaler. Edit workloadautoscaler_types.go to remove/update
-	Foo string `json:"foo,omitempty"`
+// TargetSpec defines the target resources for the autoscaler
+type TargetSpec struct {
+	Selector          *metav1.LabelSelector `json:"selector,omitempty"`
+	ResourceReference *ResourceReference    `json:"resourceReference,omitempty"`
+}
+
+// VPAReference defines the reference to the VerticalPodAutoscaler
+type VPAReference struct {
+	Name      string `json:"name"`
+	Namespace string `json:"namespace"`
+}
+
+// ResourceReference defines a reference to a Kubernetes resource
+type ResourceReference struct {
+	APIVersion string `json:"apiVersion"`
+	Kind       string `json:"kind"`
+	Name       string `json:"name"`
+}
+
+// UpdateWindow defines a time window for allowed updates
+type UpdateWindow struct {
+	DayOfWeek string `json:"dayOfWeek"`
+	StartTime string `json:"startTime"`
+	EndTime   string `json:"endTime"`
+	TimeZone  string `json:"timeZone"`
+}
+
+// Duration defines a duration in Go Duration format
+type Duration struct {
+	Duration string `json:"duration"`
 }
 
 // WorkloadAutoscalerStatus defines the observed state of WorkloadAutoscaler
 type WorkloadAutoscalerStatus struct {
-	// INSERT ADDITIONAL STATUS FIELD - define observed state of cluster
-	// Important: Run "make" to regenerate code after modifying this file
+	CurrentStatus       string             `json:"currentStatus,omitempty"`
+	TargetedResource    TargetedResource   `json:"targetedResource,omitempty"`
+	LastUpdated         metav1.Time        `json:"lastUpdated,omitempty"`
+	CurrentRequests     ResourceRequests   `json:"currentRequests,omitempty"`
+	RecommendedRequests ResourceRequests   `json:"recommendedRequests,omitempty"`
+	SkippedUpdates      bool               `json:"skippedUpdates,omitempty"`
+	SkipReason          string             `json:"skipReason,omitempty"`
+	StepSize            ResourceRequests   `json:"stepSize,omitempty"`
+	Errors              []string           `json:"errors,omitempty"`
+	UpdateCount         int32              `json:"updateCount,omitempty"`
+	Conditions          []metav1.Condition `json:"conditions,omitempty"`
+}
+
+// TargetedResource defines the targeted resource information
+type TargetedResource struct {
+	Kind string `json:"kind"`
+	Name string `json:"name"`
+}
+
+// ResourceRequests defines the resource requests for CPU and Memory
+type ResourceRequests struct {
+	CPU    string `json:"cpu,omitempty"`
+	Memory string `json:"memory,omitempty"`
 }
 
 // +kubebuilder:object:root=true
--- internal/controller/workloadautoscaler_controller.go
+++ internal/controller/workloadautoscaler_controller.go
@@ -33,9 +33,9 @@ type WorkloadAutoscalerReconciler struct {
 	Scheme *runtime.Scheme
 }
 
-// +kubebuilder:rbac:groups=autoscaling.k8s.io.autoscaling.k8s.io,resources=workloadautoscalers,verbs=get;list;watch;create;update;patch;delete
-// +kubebuilder:rbac:groups=autoscaling.k8s.io.autoscaling.k8s.io,resources=workloadautoscalers/status,verbs=get;update;patch
-// +kubebuilder:rbac:groups=autoscaling.k8s.io.autoscaling.k8s.io,resources=workloadautoscalers/finalizers,verbs=update
+// +kubebuilder:rbac:groups=autoscaling.k8s.io,resources=workloadautoscalers,verbs=get;list;watch;create;update;patch;delete
+// +kubebuilder:rbac:groups=autoscaling.k8s.io,resources=workloadautoscalers/status,verbs=get;update;patch
+// +kubebuilder:rbac:groups=autoscaling.k8s.io,resources=workloadautoscalers/finalizers,verbs=update
 
 // Reconcile is part of the main kubernetes reconciliation loop which aims to
 // move the current state of the cluster closer to the desired state.
